753c190fd26c7b6d5e482dc75c27dd61a503514f
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-09-09 20:52:13 +0100

    made new changes...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
+import Link from 'next/link'
+
 import Hero from '@/components/Hero'
 import Layout from '@/components/Layout'
 
-import { useAppSelector } from '@/redux/Store'
 import Modal from '@/types/Modal'
 
-import Link from 'next/link'
-
 export default function Home() {
-  // const isModalOpen = useAppSelector((state) => state.modalReducer.modal.isOpen)
   const NavItems = ['shop', 'on sale', 'new arrivals', 'brands']
 
   return (
@@ -15,9 +13,10 @@ export default function Home() {
       <Hero />
 
       <Modal
-        modalClass='w-[90%] mx-auto top-5 left-4 p-5 z-20 absolute bg-white '
+        modalClass='w-[80%] mx-auto top-0 left-0 p-5 z-20 rounded-xl h-screen '
         contentClass='flex flex-col justify-start items-start gap-5 capitalize font-semibold'
       >
+      
         {NavItems.map((items, key) => {
           return (
             <Link href={items} key={key}>
--- src/types/Modal.tsx
+++ src/types/Modal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import Image from 'next/image'
 import clsx from 'clsx'
 
@@ -10,16 +10,12 @@ import { closeModal } from '@/redux/Slice/ModalSlice'
 import { AppDispatch, useAppSelector } from '@/redux/Store'
 
 type ModalProps = {
-  // isOpen: boolean
-  // onClose: () => void
   children: React.ReactNode
   modalClass?: string
   contentClass?: string
 }
 
 const Modal = ({
-  // isOpen,
-  // onClose,
   children,
   modalClass,
   contentClass,
@@ -38,22 +34,26 @@ const Modal = ({
   return (
     <div
       className={clsx(
-        'xl:hidden md:flex sm:flex absolute bg-white top-0 left-0 shadow-2xl rounded-xl justify-between items-start ',
+        'xl:hidden md:flex sm:flex flex-col gap-5 absolute bg-white top-0 left-0 shadow-xl rounded-2xl ',
         modalClass
       )}
     >
-      <div className={clsx('', contentClass)}>{children}</div>
-      <button
-        onClick={handleClose}
-        // style={{
-        //   display: 'flex',
-        //   marginLeft: 'auto',
-        //   justifyContent: 'flex-end',
-        //   alignItems: 'flex-end',
-        // }}
-      >
-        <Image src={close} width={20} height={20} alt='close' />
-      </button>
+      <div className='flex justify-between items-center '>
+        <h1 className='text-2xl font-extrabold uppercase'>shop.co</h1>
+        <button
+          onClick={handleClose}
+          // style={{
+          //   display: 'flex',
+          //   marginLeft: 'auto',
+          //   justifyContent: 'flex-end',
+          //   alignItems: 'flex-end',
+          // }}
+        >
+          <Image src={close} width={20} height={20} alt='close' />
+        </button>
+      </div>
+
+      <div className={clsx(contentClass)}>{children}</div>
     </div>
   )
 }
