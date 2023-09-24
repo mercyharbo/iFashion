068c9faa20b6a042d2ccdef558d4d44b2f92978d
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-09-24 23:58:33 +0100

    Made improvement...

--- src/app/casual/page.tsx
+++ src/app/casual/page.tsx
@@ -7,16 +7,28 @@ import clsx from 'clsx'
 import Product from '@/types/Product'
 import ProductJSON from '@/components/Product.json'
 import { CalculateAverageRating } from '../utils/avarageRatings'
+import Button from '@/types/Button'
 
 type casualProps = {
   categoryWrapper?: string
 }
 
+const categories = ['t-shirts', 'shorts', 'shirts', 'hoodie', 'jean']
+
 function CasualCategory() {
   return (
-    <main className='flex justify-center items-center 2xl:gap-10 2xl:p-10'>
-      <div className='flex flex-col justify-start items-start gap-5 2xl:w-[20%] '>
-        category
+    <main className='flex justify-center items-start 2xl:gap-10 2xl:p-10'>
+      <div className='flex flex-col justify-start items-start gap-5 border-[1px] rounded-xl p-2 2xl:w-[20%] '>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-2xl font-semibold'>Filters</h1>
+          <span className=''>icon</span>
+        </div>
+        <hr />
+        <div className='flex flex-col justify-start items-start gap-5'>
+          {categories.map((category, index) => {
+            return <Button key={index} type='button' title={category} />
+          })}
+        </div>
       </div>
 
       <div className='flex flex-col justify-between items-start gap-5 2xl:w-[80%] '>
@@ -28,7 +40,7 @@ function CasualCategory() {
           </div>
         </div>
 
-        <div className='grid 2xl:grid-cols-3 xl:grid-cols-3 xl:content-start xl:place-items-start xl:gap-5'>
+        <div className='grid 3xl:grid-cols-4 2xl:grid-cols-4 2xl:gap-2 xl:grid-cols-3 xl:content-start xl:place-items-start xl:gap-5'>
           {ProductJSON.products.map((item, index) => {
             const avarageRating = CalculateAverageRating(item?.reviews)
 
@@ -41,9 +53,9 @@ function CasualCategory() {
                   productImage={item.productImage}
                   ratings={avarageRating}
                   productClass={clsx(
-                    ` inline-block xl:w-[300px] xl:mx-3 md:w-[250px] md:mx-3 sm:w-[250px] sm:mx-2 border-[1px] rounded-xl cursor-pointer hover:scale-105 ease-in-out duration-300 `
+                    ` inline-block 3xl:w-[300px] 2xl:w-[250px] xl:w-[300px] xl:mx-3 md:w-[250px] md:mx-3 sm:w-[250px] sm:mx-2 border-[1px] rounded-xl cursor-pointer hover:scale-105 ease-in-out duration-300 `
                   )}
-                  starStyling='text-2xl'
+                  starStyling='text-xl'
                 />
               </Link>
             )
