418636a50f707e0e16027409248ba8918ba34d8a
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-09-25 02:00:47 +0100

    made improvement...

--- src/app/casual/page.tsx
+++ src/app/casual/page.tsx
@@ -17,8 +17,16 @@ const categories = ['t-shirts', 'shorts', 'shirts', 'hoodie', 'jean']
 
 function CasualCategory() {
   return (
-    <main className='flex justify-center items-start 2xl:gap-10 2xl:p-10'>
-      <div className='flex flex-col justify-start items-start gap-5 border-[1px] rounded-xl p-2 2xl:w-[20%] '>
+    <main
+      className={clsx(
+        'flex justify-center items-start 2xl:gap-10 2xl:p-10 md:p-5 sm:p-5'
+      )}
+    >
+      <div
+        className={clsx(
+          '2xl:w-[20%] xl:flex xl:flex-col xl:justify-start xl:items-start md:hidden sm:hidden gap-5 border-[1px] rounded-xl p-2   '
+        )}
+      >
         <div className='flex justify-between items-center'>
           <h1 className='text-2xl font-semibold'>Filters</h1>
           <span className=''>icon</span>
@@ -31,16 +39,28 @@ function CasualCategory() {
         </div>
       </div>
 
-      <div className='flex flex-col justify-between items-start gap-5 2xl:w-[80%] '>
-        <div className='flex justify-between items-center w-full '>
-          <h1 className='text-2xl font-semibold capitalize '>casual</h1>
-          <div className='flex justify-center items-center gap-5'>
+      <div
+        className={clsx(
+          'flex flex-col justify-between items-start gap-5 2xl:w-[80%] md:w-full sm:w-full '
+        )}
+      >
+        <div
+          className={clsx(
+            'flex justify-between w-full xl:items-center xl:flex-row md:flex-row sm:flex-col sm:gap-2 '
+          )}
+        >
+          <h1 className={clsx('text-2xl font-semibold capitalize ')}>casual</h1>
+          <div className={clsx('flex justify-center items-center gap-5')}>
             <span className='text-sm'>Showing 1-10 of 100 Products</span>
             <span className='text-sm capitalize'>sort by </span>
           </div>
         </div>
 
-        <div className='grid 3xl:grid-cols-4 2xl:grid-cols-4 2xl:gap-2 xl:grid-cols-3 xl:content-start xl:place-items-start xl:gap-5'>
+        <div
+          className={clsx(
+            'grid 3xl:grid-cols-4 2xl:grid-cols-4 2xl:gap-2 xl:grid-cols-3 xl:content-start xl:place-items-start xl:gap-5 md:grid-cols-3 md:gap-3 md:content-center md:place-items-center sm:grid-cols-1 sm:gap-5'
+          )}
+        >
           {ProductJSON.products.map((item, index) => {
             const avarageRating = CalculateAverageRating(item?.reviews)
 
@@ -53,7 +73,7 @@ function CasualCategory() {
                   productImage={item.productImage}
                   ratings={avarageRating}
                   productClass={clsx(
-                    ` inline-block 3xl:w-[300px] 2xl:w-[250px] xl:w-[300px] xl:mx-3 md:w-[250px] md:mx-3 sm:w-[250px] sm:mx-2 border-[1px] rounded-xl cursor-pointer hover:scale-105 ease-in-out duration-300 `
+                    ` inline-block 3xl:w-[300px] 2xl:w-[250px] xl:w-[300px] md:w-[250px] sm:w-full border-[1px] rounded-xl cursor-pointer `
                   )}
                   starStyling='text-xl'
                 />
