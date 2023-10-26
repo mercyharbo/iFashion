2fbd4a06f98afda500631163e5a3d3d5ac1cae4b
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-10-26 17:42:40 +0100

    Add change password features

--- src/app/(main)/profile/password/page.tsx
+++ src/app/(main)/profile/password/page.tsx
@@ -3,6 +3,10 @@
 import React, { useState } from 'react'
 import { Field, Form, Formik, FormikHelpers } from 'formik'
 import { BsEyeFill } from 'react-icons/bs'
+import { toast } from 'react-toastify'
+import { useDispatch } from 'react-redux'
+import { AppDispatch, useAppSelector } from '@/redux/Store'
+import { setIsSubmitting } from '@/redux/Slice/UserSlice'
 
 interface Values {
   oldPassword: string
@@ -10,7 +14,43 @@ interface Values {
 }
 
 export default function Password() {
+  const dispatch = useDispatch<AppDispatch>()
   const [isPasswordHide, setIsPasswordHide] = useState(true)
+  const user = useAppSelector((state) => state.userProfile.user)
+  const isSubmitting = useAppSelector((state) => state.userProfile.isSubmitting)
+
+  const handleUpdateProfile = async (values: Values) => {
+    dispatch(setIsSubmitting(true))
+    try {
+      const token = localStorage.getItem('token')
+      const response = await fetch(
+        `${process.env.BASE_URL}/user/change-password`,
+        {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(values),
+        }
+      )
+
+      const data = await response.json()
+      if (data.success === true) {
+        toast.success(data.message)
+        dispatch(setIsSubmitting(false))
+
+        window.location.reload()
+      } else {
+        toast.error(data.message)
+        dispatch(setIsSubmitting(false))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  console.log(user?.email, 'as email...')
 
   return (
     <main className='flex flex-col justify-start items-start gap-5 bg-white shadow-2xl rounded-xl p-5 xl:w-[50%] '>
@@ -22,8 +62,7 @@ export default function Password() {
           newPassword: '',
         }}
         onSubmit={(values: Values, {}: FormikHelpers<Values>) => {
-          //   handleUpdateProfile(values)
-          // alert('Form submitted with values: ' + JSON.stringify(values))
+          handleUpdateProfile(values)
         }}
       >
         <Form className='py-5 flex flex-col justify-start items-start gap-5 w-full'>
@@ -39,7 +78,7 @@ export default function Password() {
             />
             <button
               type='button'
-              onClick={() => setIsPasswordHide(true)}
+              onClick={() => setIsPasswordHide(!isPasswordHide)}
               className='absolute top-[3rem] right-2'
             >
               <BsEyeFill className='text-xl' />
@@ -69,7 +108,7 @@ export default function Password() {
             type='submit'
             className='bg-black text-white py-3 px-5 rounded-md ml-auto hover:bg-[red] '
           >
-            {/* {isSubmitting ? 'Updating...' : 'Update'} */}
+            {isSubmitting ? 'Updating...' : 'Update'}
           </button>
         </Form>
       </Formik>
