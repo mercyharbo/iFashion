b0e94efa9d27fcb710917fa18ae8f4ebfdbc5f7c
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-11-07 17:04:57 +0100

    Added dashboard nav

--- src/app/(Dashboard)/Layout.tsx
+++ src/app/(Dashboard)/Layout.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function Layout() {
+import DashboardNav from '@/components/DashboardNav'
+
+import '@/app/globals.css'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>Layout</div>
+    <main className='flex w-full h-screen'>
+      <DashboardNav />
+      <section className='xl:w-[80%] md:w-full sm:w-full '>{children}</section>
+    </main>
   )
 }
---
+++ src/app/(Dashboard)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return <main className='xl:p-10'>Dashboard</main>
+}
