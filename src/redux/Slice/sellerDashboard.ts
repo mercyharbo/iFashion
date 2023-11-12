cba5177b057a7207096165be9914625440e0339b
Adigun Praise <110107990+Praisedoumar@users.noreply.github.com>
2023-11-12 02:54:23 +0100

    Implemented seller dashboard table

---
+++ src/redux/Slice/sellerDashboard.ts
@@ -0,0 +1,136 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
+
+type product = {
+  _id: string
+  title: string
+  description: string
+  category: string
+  discount: number
+  price: number
+  colors: string[]
+  available_sizes: string[]
+  reviews: []
+  images: string[]
+  related: [
+    {
+      _id: string
+      title: string
+      discount: number
+      price: number
+      reviews: []
+      images: string[]
+    }
+  ]
+  faq: []
+  createdDate: string
+  inStock: number
+}
+
+type InitialState = {
+  sellerproduct: [
+    {
+      _id: string
+      title: string
+      description: string
+      category: string
+      discount: number
+      price: number
+      colors: string[]
+      available_sizes: string[]
+      reviews: []
+      images: string[]
+      related: [
+        {
+          _id: string
+          title: string
+          discount: number
+          price: number
+          reviews: []
+          images: string[]
+        }
+      ]
+      faq: []
+      createdDate: string
+      inStock: number
+    }
+  ]
+  error: string | null
+  isLoading: boolean
+}
+
+const initialState: InitialState = {
+  sellerproduct: [],
+  error: null,
+  isLoading: true,
+}
+
+// Create an async thunk to fetch user data
+export const getSellerProduct = createAsyncThunk(
+  'product/getSellerProduct',
+  async (_, { dispatch }) => {
+    dispatch(setIsLoading(true))
+    try {
+      const token = localStorage.getItem('token') // Get the token from localStorage
+      const headers = new Headers({
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      })
+
+      const options = {
+        method: 'GET',
+        headers,
+      }
+
+      const response = await fetch(
+        `${process.env.BASE_URL}/products/seller`,
+        options
+      )
+
+      if (response.ok) {
+        const productData = await response.json()
+        dispatch(setIsLoading(false))
+        return productData.products
+      } else {
+        const errorData = await response.json()
+        const errorMessage = errorData.message
+        dispatch(setIsLoading(false))
+        toast.error(errorMessage, {
+          position: 'top-right',
+          autoClose: 5000, // Adjust as needed
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        })
+
+        return toast.error(errorMessage)
+      }
+    } catch (error) {
+      return 'An error occurred whe=ile trying to fetch user'
+    }
+  }
+)
+
+const sellerSlice = createSlice({
+  name: 'seller',
+  initialState,
+  reducers: {
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getSellerProduct.fulfilled, (state, action) => {
+        state.sellerproduct = action.payload
+        state.error = null
+      })
+      .addCase(getSellerProduct.rejected, (state, action) => {
+        state.error = action.payload as string
+      })
+  },
+})
+
+export const { setIsLoading } = sellerSlice.actions
+export default sellerSlice.reducer
