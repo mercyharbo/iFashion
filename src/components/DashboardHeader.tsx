18646f54127202d16d9c4f6bf422f3bea2a009db
Afolabi Ridwan Damilare <damilare791@gmail.com>
2023-11-20 02:30:08 +0100

    Made changes on the UI

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,20 +1,23 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { HiOutlineBars3BottomRight } from 'react-icons/hi2'
+import { HiMagnifyingGlass, HiOutlineBars3BottomRight } from 'react-icons/hi2'
+import { IoIosArrowDown } from 'react-icons/io'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { useDispatch } from 'react-redux'
 
 import { AppDispatch, useAppSelector } from '@/redux/Store'
 import {
+  BiBell,
   BiHome,
   BiLogOut,
   BiShoppingBag,
   BiStore,
   BiUser,
 } from 'react-icons/bi'
-import clsx from 'clsx'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { useDispatch } from 'react-redux'
+
 import { fetchUserProfile, setToken } from '@/redux/Slice/UserSlice'
 
 const navItems = [
@@ -39,11 +42,10 @@ export default function DashboardHeader() {
   }, [dispatch])
 
   return (
-    <nav className='relative flex justify-between items-center bg-white shadow-2xl h-[4rem] w-full xl:px-10 md:px10 sm:px-5 '>
-      <h1 className='font-semibold capitalize text-2xl xl:hidden md:flex sm:flex'>
-        shop.co
-      </h1>
-      <div className='flex justify-start items-start gap-5'>
+    <nav className='relative flex justify-between items-center bg-white shadow-lg h-[4rem] w-full xl:px-10 md:px-10 sm:px-5 '>
+      <h1 className='font-semibold capitalize text-2xl '>Dashboard</h1>
+
+      <div className='xl:flex md:flex sm:hidden justify-start items-center gap-5'>
         {/* <Image
           src={user?.profile_picture || ''}
           alt={user?.firstName || ''}
@@ -51,45 +53,63 @@ export default function DashboardHeader() {
           height={100}
           className='object-cover object-top rounded-full'
         /> */}
-        <span className='font-semibold'>
+
+        <button type='button' className='text-2xl'>
+          <HiMagnifyingGlass />
+        </button>
+        <button type='button' className='text-2xl'>
+          <BiBell />
+        </button>
+        <Link
+          href='/profile'
+          className='font-semibold flex justify-start items-center gap-2'
+        >
           {user ? `Welcome, ${user?.firstName}` : ''}
-        </span>
+          <IoIosArrowDown />
+        </Link>
       </div>
       <button
         type='button'
         onClick={() => setIsNavModal(true)}
-        className='xl:hidden md:flex sm:flex text-2xl'
+        className='xl:hidden md:hidden sm:flex text-2xl'
       >
         <HiOutlineBars3BottomRight />
       </button>
 
       {isNavOpen && (
-        <div className='flex flex-col justify-start items-start gap-5 bg-white rounded-2xl w-[90%] absolute top-2 left-2 '>
-          <div className='flex flex-col justify-start items-start gap-2'>
-            {navItems.map((item) => {
-              return (
-                <Link
-                  key={item.id}
-                  href={item.href}
-                  className={clsx(
-                    'py-3 w-full capitalize pl-2 flex justify-start items-center gap-2 rounded-md',
-                    pathname === item.href ? 'bg-blue-300  ' : ''
-                  )}
-                >
-                  <span className='text-2xl'> {item.icon}</span>
-                  {item.title}
-                </Link>
-              )
-            })}
-          </div>
+        <>
+          <div
+            onClick={() => setIsNavModal(false)}
+            className='bg-[#0000008f] fixed h-screen w-full top-0 left-0 '
+          ></div>
+          <div className='flex flex-col justify-start items-start gap-5 bg-black rounded-2xl w-[90%] absolute top-2 left-4 z-10 shadow-2xl p-5 '>
+            <div className='flex flex-col justify-start items-start gap-2 w-full'>
+              {navItems.map((item) => {
+                return (
+                  <Link
+                    key={item.id}
+                    href={item.href}
+                    className={clsx(
+                      'flex justify-start items-center gap-2 py-3 px-2 w-full rounded-xl text-white capitalize',
+                      pathname === item.href ? 'bg-white text-[black] ' : ''
+                    )}
+                  >
+                    <span className='text-2xl'> {item.icon}</span>
+                    {item.title}
+                  </Link>
+                )
+              })}
+            </div>
 
-          <button
-            type='button'
-            className='py-3 w-full capitalize pl-2 flex justify-start items-center gap-2'
-          >
-            Logout <BiLogOut />
-          </button>
-        </div>
+            <button
+              type='button'
+              className='py-3 w-full capitalize pl-2 flex justify-start items-center gap-2 text-white'
+            >
+              <BiLogOut />
+              Logout
+            </button>
+          </div>
+        </>
       )}
     </nav>
   )
